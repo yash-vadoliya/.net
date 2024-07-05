2104c49ba0dc6a81ea4be2e450c7161d1b4bc5c6
unknown <vadoliyayash1409@gmail.com>
2024-07-05 09:10:16 +0530

    jdb

---
+++ Table/Table/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Table.Models;
+
+namespace Table.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        { 
+            List<Home> list = new List<Home>() {
+            new Home() {
+                Id = 1,
+                    Name = "Yash",
+                    City = "Junagadh"
+            },
+            new Home() {
+                Id = 2,
+                    Name = "Keval",
+                    City = "Surat"
+            },
+            new Home() {
+                Id = 3,
+                    Name = "Abhi",
+                    City = "Ahmadabad"
+            },
+            };
+            ViewBag.model = list;
+            return View();
+        }
+    }
+}
---
+++ Table/Table/Models/Home.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Table.Models
+{
+    public class Home
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
